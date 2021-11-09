bf6a1404e8d746ea15d378b52f6e7ab773608696
anamartinez97m <anamartinez97m@gmail.com>
2021-11-09 21:25:59 +0100

    Added get method to service

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
 
-  constructor() { }
+  gameListFromApi: any;
+  url = 'https://www.freetogame.com/api/games';
+
+  constructor(private http: HttpClient) { }
+
+  getGameListFromAPI(): Observable<any> {
+    return this.http.get<any>(this.url)
+      .pipe(
+        map((data: any) => this.gameListFromApi = data)
+      );
+  }
 }
